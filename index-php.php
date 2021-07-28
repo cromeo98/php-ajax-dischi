e317009cb4ede146eb2db3c86c69be009669ee6b
cromeo98 <romeo.christian98@gmail.com>
2021-07-28 17:03:05 +0200

    end milestone 1 (no formats)

--- index-php.php
+++ index-php.php
@@ -1,6 +1,9 @@
 <?php
-
+    include __DIR__ . '/partials/db.php';
+    include __DIR__ . '/partials/functions.php';
 ?>
+
+<!-- Stampare a schermo una decina di dischi musicali -->
 
 <!DOCTYPE html>
 <html lang="en">
@@ -9,8 +12,54 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>php ajax dischi</title>
+    <!-- link to my framework -->
+    <link rel="stylesheet" href="css/framework.css">
+    <!-- link to my css -->
+    <link rel="stylesheet" href="css/style.css">
+
 </head>
 <body>
+
+    <header>
+
+    </header>
+
+    <main>
+
+        <div class="container">
+
+            <div class="row">
+
+                <?php foreach($albums as $album){ ?>
+
+                    <div class="ms_one-of-five-cols p-1rem txt-cen">
+
+                        <h3> <?php echo $album['title'] ?> </h3>
+
+                        <div class="ms_img-container">
+                            <img src="<?php echo $album['poster'] ?>" alt="<?php echo $album['title'] ?> poster">
+                        </div>
+
+                        <div class="ms_info">
+                            <h5> <?php echo $album['author'] ?> </h5>
+                            <h5> <?php echo $album['year'] ?> </h5>
+                        </div>
+
+                    </div>
+
+
+                <?php } ?>
+
+
+            </div>
+
+        </div>
+
+    </main>
     
+    <footer>
+
+    </footer>
+
 </body>
 </html>
---
+++ partials/db.php
@@ -0,0 +1,63 @@
+<?php
+    $albums = [
+        [
+            'title' => 'Zoo',
+            'author' => 'Russ',
+            'year' => 2018,
+            'poster' => 'https://upload.wikimedia.org/wikipedia/en/7/75/Russ_-_Zoo.png'
+        ],
+        [
+            'title' => 'New Jersey',
+            'author' => 'Bon Jovi',
+            'year' => 1988,
+            'poster' => 'https://images-na.ssl-images-amazon.com/images/I/51sBr4IWDwL.jpg'
+        ],
+        [
+            'title' => 'To Pimp a Butterfly',
+            'author' => 'Kendrick Lamar',
+            'year' => 2015,
+            'poster' => 'https://www.rollingstone.it/wp-content/uploads/2015/03/KENDRICCONE-e1426160992159.png'
+        ],
+        [
+            'title' => 'ASTROWORLD',
+            'author' => 'Travis Scott',
+            'year' => 2018,
+            'poster' => 'https://img.discogs.com/rtWY1w2laqQYSLWgfTs959UVDX4=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb():quality(40)/discogs-images/R-12773291-1541681041-1214.jpeg.jpg'
+        ],
+        [
+            'title' => 'La bella musica',
+            'author' => 'Vegas Jones',
+            'year' => 2019,
+            'poster' => 'https://images.genius.com/426ef59443672ecf0d1ecf4daa0db7a8.1000x1000x1.png'
+        ],
+        [
+            'title' => 'RECOVƎRY',
+            'author' => 'Eminem',
+            'year' => 2010,
+            'poster' => 'https://rapmusicratings.files.wordpress.com/2018/09/recovery-e1536075287406.jpg'
+        ],
+        [
+            'title' => '17',
+            'author' => 'X Tentacion',
+            'year' => 2017,
+            'poster' => 'https://yt3.ggpht.com/ytc/AKedOLTPRzHhJiIWG1tXD9Jl2NIHg8PY9QHKer5DmwFetw=s900-c-k-c0x00ffffff-no-rj'
+        ],
+        [
+            'title' => 'L\'unico compromesso',
+            'author' => 'Gemitaiz',
+            'year' => 2013,
+            'poster' => 'https://i0.wp.com/www.bad-boy.it/wp-content/uploads/2013/06/gemitaiz-l-unico-compromesso-cd-cover-560x500.jpg?resize=560%2C500'
+        ],
+        [
+            'title' => 'Hybrid Theory',
+            'author' => 'Linkin Park',
+            'year' => 2000,
+            'poster' => 'https://www.impericon.com/432x624x90/media/catalog/product/2/0/20200818_linkin_park_ht20_cd_lg.jpg'
+        ],
+        [
+            'title' => 'Americana',
+            'author' => 'The Offspring',
+            'year' => 1998,
+            'poster' => 'https://m.media-amazon.com/images/I/81CAxmSTEgL._SS500_.jpg'
+        ],
+    ];
